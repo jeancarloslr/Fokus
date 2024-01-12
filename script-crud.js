46fbb5be199b123a1cf1a2198fda6007fb2b0731
Jean Carlos <130818271+jeancarloslr@users.noreply.github.com>
2024-01-12 17:01:45 -0300

    armazenando task na localStorage

--- script-crud.js
+++ script-crud.js
@@ -1,6 +1,20 @@
 const btnAddTask = document.querySelector('.app__button--add-task');
 const formTasks = document.querySelector('.app__form-add-task');
+const textArea = document.querySelector('.app__form-textarea');
+
+const tarefas = [];
 
 btnAddTask.addEventListener('click', () =>{
     formTasks.classList.toggle('hidden')
+});
+
+formTasks.addEventListener('submit', (evento) =>{
+    evento.preventDefault(); //vai prevenir o comportamento padrão do navegador.
+    const tarefa = {
+        descricao: textArea.value
+    }
+
+    tarefas.push(tarefa);
+    localStorage.setItem('tarefas', tarefas);
+    
 });
